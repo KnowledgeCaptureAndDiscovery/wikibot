6eff1e35ef39303c5f9dfbd631d7a9c9caa886ac
Neha Suvarna Sanjeev <suvarnas@usc.edu>
2017-04-13 19:48:40 -0700

    Login class. The class enables login with a username password and also stores the session id

---
+++ bot1/src/main/java/com/wikibot/entity/Login.java
@@ -0,0 +1,67 @@
+package com.wikibot.entity;
+
+import static com.wikibot.app.Constants.FORMAT_AND_API;
+import static com.wikibot.app.Constants.WIKI_NAME;
+
+import org.json.JSONObject;
+
+import com.wikibot.app.Utils;
+
+public class Login {
+	String lgname;
+	String lgpassword;
+	String lgcookie;
+	
+	
+	public Login(String lgname, String lgpassword){
+		this.lgname = lgname;
+		this.lgpassword = lgpassword;
+	}
+
+
+	public String getLgcookie() {
+		return lgcookie;
+	}
+
+
+	public void setLgcookie(String lgcookie) {
+		this.lgcookie = lgcookie;
+	}
+	
+	public void login(){
+		String tokenQuery = WIKI_NAME
+				+ "api.php?action=login&format=json&lgname=" + this.lgname;
+		/* Cookie parameter is empty because no session is set */
+		JSONObject getToken = Utils.doPOSTJSON(tokenQuery, "");
+
+		String token = getToken.getJSONObject("login").get("token").toString();
+		String sessionid = getToken.getJSONObject("login").get("sessionid").toString();
+		String cookieprefix = getToken.getJSONObject("login").get("cookieprefix").toString();
+
+		/* Set the cookie string to be sent in the subsequent post requests */
+		setLgcookie(cookieprefix + "=" + sessionid);
+
+		/* Login along with the token id */
+		String loginQuery = tokenQuery + "&lgpassword=" + this.lgpassword + "&lgtoken=" + token;
+		JSONObject login = Utils.doPOSTJSON(loginQuery, this.lgcookie);
+		System.out.println(login.toString());
+
+		/* Assertion to check if the login is successful */
+		String user = WIKI_NAME + FORMAT_AND_API + "&assert=user";
+		System.out.println(Utils.doPOSTJSON(user, this.lgcookie).toString());
+	}	
+	
+	public void edit(){
+		String tokenQuery2 = WIKI_NAME + FORMAT_AND_API
+				+ "&format=json&meta=tokens";
+		JSONObject getToken2 = Utils.doPOSTJSON(tokenQuery2, this.getLgcookie());
+		String token2 = getToken2.getJSONObject("query")
+				.getJSONObject("tokens").get("csrftoken").toString();
+		System.out.println(token2);
+
+		String editQuery = WIKI_NAME
+				+ "api.php?action=edit&title=Test&text=Hello&format=json";
+		JSONObject edit = Utils.postFuncWithParams(editQuery, this.getLgcookie(), token2);
+		System.out.println(edit);
+	}
+}
